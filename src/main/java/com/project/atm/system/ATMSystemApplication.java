23e02edd7b589c8aac4dfe2afa780d4ab7b5b4d7
Ankit Chobdar <ankitchobdar@gmail.com>
2021-12-29 08:41:25 +0530

    Initial commit

---
+++ src/main/java/com/project/atm/system/ATMSystemApplication.java
@@ -0,0 +1,11 @@
+package com.project.atm.system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ATMSystemApplication {
+    public static void main(String args[]) {
+        SpringApplication.run(ATMSystemApplication.class, args);
+    }
+}
